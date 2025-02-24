d7a675df0d64de6f73b1007bdae3d143502ed366
unclebob482 <danelege@gmail.com>
2025-02-24 15:43:07 -0600

    Add files via upload

---
+++ Download.bat
@@ -0,0 +1,24 @@
+@echo off
+echo Installing necessary Python packages...
+pip install torch
+pip install torchvision
+pip install matplotlib>=3.2.2
+pip install numpy>=1.18.5
+pip install opencv-python>=4.1.2
+pip install Pillow
+pip install PyYAML>=5.3.1
+pip install scipy>=1.4.1
+pip install tqdm>=4.41.0
+pip install tensorboard>=2.4.1
+pip install seaborn>=0.11.0
+pip install pandas
+pip install mss
+pip install pygame
+pip install pynput
+pip install pywin32
+pip install requests
+pip install wheel
+pip install termcolor
+
+echo All packages installed successfully!
+pause
---
+++ Lunar lancher.bat
@@ -0,0 +1,5 @@
+@echo off
+set SCRIPT_PATH=%~dp0
+echo Running Lunar script...
+python "%SCRIPT_PATH%\Silent-Soft-Aim-main\lunar.py"
+pause
